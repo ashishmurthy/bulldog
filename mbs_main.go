22a357d99d91bd7d3baeee140cb3c8cc7b263b98
ashishmurthy <ashishmurthy@gmail.com>
2016-10-01 23:50:26 +0200

    Update mbs_main.go

--- mbs_main.go
+++ mbs_main.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
-
+	"strings"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
@@ -67,6 +67,104 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 
 	return nil, nil
 }
+
+// Invoke code for the MBS blockchain
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	if function == "CreateTranche" {
+		// Deletes an entity from its state
+		return t.CreateTranche(stub, args)
+	}
+
+	var A, B string    // Entities
+	var Aval, Bval int // Asset holdings
+	var X int          // Transaction value
+	var err error
+
+	if len(args) != 3 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3")
+	}
+
+	A = args[0]
+	B = args[1]
+
+	// Get the state from the ledger
+	// TODO: will be nice to have a GetAllState call to ledger
+	Avalbytes, err := stub.GetState(A)
+	if err != nil {
+		return nil, errors.New("Failed to get state")
+	}
+	if Avalbytes == nil {
+		return nil, errors.New("Entity not found")
+	}
+	Aval, _ = strconv.Atoi(string(Avalbytes))
+
+	Bvalbytes, err := stub.GetState(B)
+	if err != nil {
+		return nil, errors.New("Failed to get state")
+	}
+	if Bvalbytes == nil {
+		return nil, errors.New("Entity not found")
+	}
+	Bval, _ = strconv.Atoi(string(Bvalbytes))
+
+	// Perform the execution
+	X, err = strconv.Atoi(args[2])
+	Aval = Aval - X
+	Bval = Bval + X
+	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
+
+	// Write the state back to the ledger
+	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
+	if err != nil {
+		return nil, err
+	}
+
+	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+// Deletes an entity from state
+func (t *SimpleChaincode) CreateTranche(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var A, B, C, Aval, Bval, Cval, retstr string    // 
+	
+	if len(args) != 3 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3")
+	}
+
+	A := args[0] //Loan1
+	B := args[1] //Loan2
+	C := args[2] //Loan3
+	
+	Avalbytes, err := stub.GetState(A)
+	if err != nil {
+		return nil, errors.New("Failed to get state")
+	}
+	if Avalbytes == nil {
+		return nil, errors.New("Entity not found")
+	}
+	Aval = string(Avalbytes))
+	out := strings.split(Aval, "|")
+	rating := strconv.Atoi(out[3])
+	if rating >=7 then retstr = "Sr. Secured"
+	
+	jsonResp := "{\"Loan\":\"" + A + "\",\"Tranche\":\"" + retstr + "\"}"
+	fmt.Printf("Query Response:%s\n", jsonResp)
+	return Avalbytes, nil
+}
+
+	// Delete the key from the state in ledger
+	err := stub.DelState(A)
+	if err != nil {
+		return nil, errors.New("Failed to delete state")
+	}
+
+	return nil, nil
+}
+
 
 func main() {
 	err := shim.Start(new(SimpleChaincode))
